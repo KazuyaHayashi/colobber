4ef37171423601a2990aa6f9c74f8df95e78d19c
Kazuya Hayashi <pumpkin.brownies@gmail.com>
2020-08-17 14:02:38 +0900

    Implement alpha beta search

--- src/cpu.py
+++ src/cpu.py
@@ -1,9 +1,10 @@
 from typing import List
 
 from .board import Move
-from .search import min_max_search
+from .search import min_max_search, alpha_beta_search
 
 
 def cpu_player(stone: int, board: List[List[int]]) -> Move:
-    move = min_max_search(stone, board, 1)
+    #move = min_max_search(stone, board, 2)
+    move = alpha_beta_search(stone, board, 2)
     return move
--- src/search.py
+++ src/search.py
@@ -5,6 +5,10 @@ from .evaluate import MIN_SCORE, MAX_SCORE, evaluate
 from . import peek
 
 
+INITIAL_ALPHA_SCORE = MIN_SCORE * 2
+INITIAL_BETA_SCORE = MAX_SCORE * 2
+
+
 def min_max_search(
     stone: int, board: List[List[int]], depth: int, mine_turn=True
 ) -> Move:
@@ -48,4 +52,54 @@ def min_max_search(
                 min_score = evaluated_move.score
                 move.score = evaluated_move.score
                 candidate_move = move
+        return candidate_move
+
+
+def alpha_beta_search(
+    stone: int, board: List[List[int]], depth: int, mine_turn=True, alpha=INITIAL_ALPHA_SCORE, beta=INITIAL_BETA_SCORE
+) -> Move:
+    if depth <= 0:
+        peek.count_evaluated_moves += 1
+        return Move(score=evaluate(board))
+
+    if has_finished(board):
+        if mine_turn:
+            return Move(score=MIN_SCORE)
+        else:
+            return Move(score=MAX_SCORE)
+
+    all_moves = scan_all_moves(stone, board)
+
+    candidate_move = Move(score=0)
+    if mine_turn:
+        for move in all_moves:
+            virtual_board = deepcopy(board)
+            do_move(move, stone, virtual_board)
+            opponent_stone = get_enemy_stone(stone)
+            evaluated_move = alpha_beta_search(
+                opponent_stone, virtual_board, depth - 1, not mine_turn, alpha, beta
+            )
+            if evaluated_move.score > alpha:
+                alpha = evaluated_move.score
+                move.score = evaluated_move.score
+                candidate_move = move
+                if alpha >= beta:
+                    #print(f"ALPHA Cutting. alpha: {alpha}, beta: {beta}")
+                    break
+        return candidate_move
+    else:
+        for move in all_moves:
+            virtual_board = deepcopy(board)
+            do_move(move, stone, virtual_board)
+            opponent_stone = get_enemy_stone(stone)
+            evaluated_move = alpha_beta_search(
+                opponent_stone, virtual_board, depth - 1, not mine_turn, alpha, beta
+            )
+            if evaluated_move.score < beta:
+                beta = evaluated_move.score
+                move.score = evaluated_move.score
+                candidate_move = move
+                if beta <= alpha:
+                    #print(f"BETA Cutting. alpha: {alpha}, beta: {beta}")
+                    break
         return candidate_move
